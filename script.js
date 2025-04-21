6d9bf1117e180da8a7841625a83df999b7ef6aeb
devis-13 <timalushan@gmail.com>
2025-04-21 20:55:44 +0300

    changed styles, added selected elements

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const doors = {
 	'etalon': {
 		'et-01-pvh': {
+			title: 'Полотно, модель ЕТ-01, МДФ, гладке, покриття - плівка ПВХ (Крайка ПВХ)',
 			priceEpicentr: 4566,
 			priceRealiz: 5937,
 		},
 		'et-01-al': {
+			title: 'Полотно, модель ЕТ-01, МДФ, гладке, покриття - плівка ПВХ (Крайка Алюміній)',
 			priceEpicentr: 6330,
 			priceRealiz: 8229,
 		},
 		'et-02-17-pvh': {
+			title: 'Полотно, модель ЕТ-02 -…… - ЕТ-17, МДФ, з фрезеруванням, покриття - плівка ПВХ (Крайка ПВХ)',
 			priceEpicentr: 5226,
 			priceRealiz: 6795,
 		},
 		'et-02-17-al': {
+			title: 'Полотно, модель ЕТ-02 -…… - ЕТ-17, МДФ, з фрезеруванням, покриття - плівка ПВХ (Крайка Алюміній)',
 			priceEpicentr: 6930,
 			priceRealiz: 9009,
 		},
@@ -20,14 +24,17 @@ const doors = {
 
 	'standart': {
 		'st-1': {
+			title: 'Полотно, модель СТ-1, МДФ, покриття ПВХ',
 			priceEpicentr: 4173,
 			priceRealiz: 6126,
 		},
 		'st-2': {
+			title: 'Полотно, модель СТ-2, МДФ, покриття ПВХ',
 			priceEpicentr: 6051,
 			priceRealiz: 7866,
 		},
 		'st-3': {
+			title: 'Полотно, модель СТ-3, МДФ, покриття ПВХ',
 			priceEpicentr: 4785,
 			priceRealiz: 6219,
 		},
@@ -35,42 +42,52 @@ const doors = {
 
 	'denomination': {
 		'nl-1': {
+			title: 'Полотно, модель NL-01, МДФ, глухе, гладке, покриття - плівка ПВХ (34мм)',
 			priceEpicentr: 3249,
 			priceRealiz: 4224,
 		},
 		'nl-2-5': {
+			title: 'Полотно, модель NL-02 -.- NL-05, МДФ, глухе, гладке, покриття - плівка ПВХ чорна(34мм)',
 			priceEpicentr: 4146,
 			priceRealiz: 5392,
 		},
 		'boxTel-80': {
+			title: 'Короб телескопічний 80мм, сосна+МДФ, покриття - ПВХ (номінал)',
 			priceEpicentr: 1401,
 			priceRealiz: 1821,
 		},
 		'boxTel-100': {
+			title: 'Короб телескопічний 100мм, сосна+МДФ, покриття - ПВХ (номінал)',
 			priceEpicentr: 1626,
 			priceRealiz: 2115,
 		},
 		'boxTel-120': {
+			title: 'Короб телескопічний 120мм, сосна+МДФ, покриття - ПВХ (номінал)',
 			priceEpicentr: 1776,
 			priceRealiz: 2310,
 		},
 		'welt-80': {
+			title: 'Лиштва телескопічна 80мм, МДФ, покриття - ПВХ (номінал)',
 			priceEpicentr: 561,
 			priceRealiz: 729,
 		},
 		'welt-80-40': {
+			title: 'Лиштва телескопічна 80мм, МДФ, покриття - ПВХ (номінал, крило 40 мм)',
 			priceEpicentr: 630,
 			priceRealiz: 819,
 		},
 		'dob-100': {
+			title: 'Добір телескопічний 100 мм (номінал)',
 			priceEpicentr: 597,
 			priceRealiz: 776,
 		},
 		'dob-150': {
+			title: 'Добір телескопічний 150 мм (номінал)',
 			priceEpicentr: 825,
 			priceRealiz: 1071,
 		},
 		'dob-200': {
+			title: 'Добір телескопічний 200 мм (номінал)',
 			priceEpicentr: 1044,
 			priceRealiz: 1356,
 		},
@@ -80,34 +97,42 @@ const doors = {
 
 	'paint': {
 		'boardPrimedPaint-pr': {
+			title: 'Полотно МДФ грунтоване під фарбування (крайка грунт)',
 			priceEpicentr: 5997,
 			priceRealiz: 7797,
 		},
 		'boardPrimedPaint-al': {
+			title: 'Полотно МДФ грунтоване під фарбування (крайка алюміній)',
 			priceEpicentr: 8283,
 			priceRealiz: 10767,
 		},
 		'boardPrimedPaintInside-pr': {
+			title: 'Полотно МДФ грунтоване під фарбування Inside (крайка грунт)',
 			priceEpicentr: 6300,
 			priceRealiz: 8190,
 		},
 		'boardPrimedPaintInside-al': {
+			title: 'Полотно МДФ грунтоване під фарбування Inside (крайка алюміній)',
 			priceEpicentr: 9423,
 			priceRealiz: 12251,
 		},
 		'boardPaint': {
+			title: 'Полотно МДФ під фарбування (плівка)',
 			priceEpicentr: 5979,
 			priceRealiz: 7773,
 		},
 		'boardPaintInside': {
+			title: 'Полотно МДФ під фарбування (плівка) Inside',
 			priceEpicentr: 6915,
 			priceRealiz: 8991,
 		},
 		'concBox': {
+			title: 'Коробка прихованого монтажу',
 			priceEpicentr: 5502,
 			priceRealiz: 7152,
 		},
 		'concBoxInside': {
+			title: 'Коробка прихованого монтажу Inside',
 			priceEpicentr: 6801,
 			priceRealiz: 8841,
 		},
@@ -115,63 +140,78 @@ const doors = {
 
 	'other': {
 		'boxTel-80': {
+			title: 'Короб телескопічний 80мм, сосна+МДФ, покриття - ПВХ',
 			priceEpicentr: 1596,
 			priceRealiz: 2076,
 		},
 		'boxTel-100': {
+			title: 'Короб телескопічний 100мм, сосна+МДФ, покриття - ПВХ',
 			priceEpicentr: 1827,
 			priceRealiz: 2376,
 		},
 		'boxTel-120': {
+			title: 'Короб телескопічний 120мм, сосна+МДФ, покриття - ПВХ',
 			priceEpicentr: 1989,
 			priceRealiz: 2583,
 		},
 		'weltTel-80': {
+			title: 'Лиштва телескопічна 80мм, МДФ, покриття - ПВХ',
 			priceEpicentr: 684,
 			priceRealiz: 888,
 		},
 		'weltTel-80-40': {
+			title: 'Лиштва телескопічна 80мм, МДФ, покриття - ПВХ (крило 40 мм)',
 			priceEpicentr: 933,
 			priceRealiz: 1212,
 		},
 		'boxComp-80': {
+			title: 'Короб компланарний 80мм, сосна+МДФ, покриття - ПВХ',
 			priceEpicentr: 2169,
 			priceRealiz: 2820,
 		},
 		'weltComp-80': {
+			title: 'Лиштва компланарна 80мм, МДФ, покриття - ПВХ',
 			priceEpicentr: 1168,
 			priceRealiz: 1518,
 		},
 		'dobTel-100': {
+			title: 'Добір телескопічний 100 мм.',
 			priceEpicentr: 717,
 			priceRealiz: 933,
 		},
 		'dobTel-150': {
+			title: 'Добір телескопічний 150 мм.',
 			priceEpicentr: 1017,
 			priceRealiz: 1323,
 		},
 		'dobTel-200': {
+			title: 'Добір телескопічний 200 мм.',
 			priceEpicentr: 1332,
 			priceRealiz: 1731,
 		},
 		'fitIns-pvh': {
+			title: 'Врізка фурнітури ПВХ',
 			priceEpicentr: 462,
 			priceRealiz: 600,
 		},
 		'fitIns-al': {
+			title: 'Врізка фурнітури Алюміній',
 			priceEpicentr: 516,
 			priceRealiz: 672,
 		},
 		'paintAl': {
+			title: 'Фарбування алюмінієвої крайки',
 			priceEpicentr: 504,
 			priceRealiz: 654,
 		},
 
 		'alProf-40': {
+			title: 'Система алюмінієвих профілів для торцювання 40 мм',
 			priceEpicentr: 1947,
 			priceRealiz: 2532,
 		},
 		'alProf-56': {
+			title: 'Система алюмінієвих профілів для торцювання 56 мм (Inside)',
 			priceEpicentr: 2568,
 			priceRealiz: 3339,
 		},
@@ -180,92 +220,68 @@ const doors = {
 	},
 };
 
-switchDescription();
-// calculateAmount(doors);
+const selectedItemsList = {};
+
+let countRealiz = 0;
+let countEpicentr = 0;
+
+switchModelBlock();
 changeAmount();
 activeCheckbox();
 writePrice();
+writeLable();
 
-function switchDescription() {
-	const block = document.querySelectorAll('.door__header');
+function switchModelBlock() {
+	const titleList = document.querySelectorAll('.door__header');
 
-	block.forEach(elem => {
-		elem.addEventListener('click', () => {
-			let processListItemClasses = elem.parentElement.classList;
-			processListItemClasses.toggle('active');
+	titleList.forEach(item => {
+		item.addEventListener('click', () => {
+			item.parentElement.classList.toggle('active');
 		});
 	});
+
 
 };
 
 
 
-function calculateAmount(modelsList) {
+function calculateAmount() {
 
-	// const calculateBtn = document.querySelector('.door__sum-btn');
 	const amountEpicentr = document.querySelector('.door__amount-res--epicentr > span');
 	const amountRealiz = document.querySelector('.door__amount-res--realiz > span');
-	let selectModels;
-	let count = 0;
-	let countZac = 0;
 
-	/* 	calculateBtn.addEventListener('click', function (event) {
-			selectModels = selectCheckedModels();
-			console.log(selectModels);
-	
-			selectModels.forEach(function (item) {
-				let model = item.model;
-				console.log(model);
-				let name = item.name;
-				console.log(modelsList[model][name]);
-				count += modelsList[model][name].priceRealiz * +item.amount;
-				countZac += modelsList[model][name].priceEpicentr * +item.amount;
-			});
-	
-			console.log(count);
-			console.log(countZac);
-	
-			amountEpicentr.innerHTML = countZac;
-			amountRealiz.innerHTML = count;
-	
-			count = 0;
-			countZac = 0;
-		});
-	 */
-	selectModels = selectCheckedModels();
+	countRealiz = 0;
+	countEpicentr = 0;
 
-	selectModels.forEach(function (item) {
+	createSelectedItemsList();
+
+	for (key in selectedItemsList) {
+		let item = selectedItemsList[key];
 		let model = item.model;
 		let name = item.name;
-		count += modelsList[model][name].priceRealiz * +item.amount;
-		countZac += modelsList[model][name].priceEpicentr * +item.amount;
-	});
 
-	amountEpicentr.innerHTML = countZac;
-	amountRealiz.innerHTML = count;
+		countRealiz += doors[model][name].priceRealiz * +item.amount;
+		countEpicentr += doors[model][name].priceEpicentr * +item.amount;
+	};
 
-	count = 0;
-	countZac = 0;
+	amountEpicentr.innerHTML = countEpicentr;
+	amountRealiz.innerHTML = countRealiz;
 
+	function createSelectedItemsList() {
+		const checkedModelsItems = document.querySelectorAll('.door__item:has(input[type="checkbox"]:checked)');
 
+		for (key in selectedItemsList) {
+			delete selectedItemsList[key];
+		}
 
-
-
-	function selectCheckedModels() {
-		const doorsBlock = document.querySelectorAll('.door__item:has(input[type="checkbox"]:checked)');
-		let selectModels = [];
-
-		doorsBlock.forEach(item => {
-			let doorsModel = {
+		checkedModelsItems.forEach(item => {
+			selectedItemsList[item.id] = {
 				model: item.parentElement.parentElement.id,
 				name: item.id,
 				amount: item.querySelector('input[type="number"]').value,
 			};
-
-			selectModels.push(doorsModel);
 		});
 
-		return selectModels;
 	};
 
 };
@@ -279,14 +295,18 @@ function changeAmount() {
 		if (elem.closest('.door__value-btn--minus')) {
 			count = +elem.parentElement.querySelector('.door__amount').value;
 			elem.parentElement.querySelector('.door__amount').value = count - 1;
+			calculateAmount();
+			writeSelectedItems();
 		}
 
 		if (elem.closest('.door__value-btn--plus')) {
 			count = +elem.parentElement.querySelector('.door__amount').value;
 			elem.parentElement.querySelector('.door__amount').value = count + 1;
+			calculateAmount();
+			writeSelectedItems();
 		}
 
-		calculateAmount(doors);
+
 	});
 
 
@@ -301,32 +321,76 @@ function activeCheckbox() {
 				let inputCheck = elem.querySelector('input[type="checkbox"]');
 				if (inputCheck.checked) {
 					inputCheck.checked = false;
+					calculateAmount();
+					writeSelectedItems();
 				} else if (!inputCheck.checked) {
 					inputCheck.checked = true;
+					calculateAmount();
+					writeSelectedItems();
 				};
-			}
 
-			calculateAmount(doors);
+
+			}
 		});
 	});
 }
 
 function writePrice() {
 	const models = document.querySelectorAll('.door__item');
-	console.log(1);
 
 	models.forEach(item => {
 
-		let blockName = item.parentElement.parentElement.id;
-		let modelName = item.id;
-		let modelPrice = item.querySelector('.door__price > span');
-		console.log(blockName);
-		console.log(modelName);
-		console.log(modelPrice);
+		const blockName = item.parentElement.parentElement.id;
+		const modelName = item.id;
+		const modelPrice = item.querySelector('.door__price > span');
 
-
-		console.log(doors[blockName][modelName].priceRealiz);
 		modelPrice.innerHTML = doors[blockName][modelName].priceRealiz;
 	});
 
 }
+
+function writeLable() {
+	const models = document.querySelectorAll('.door__item');
+
+	models.forEach(item => {
+		const blockName = item.parentElement.parentElement.id;
+		const modelName = item.id;
+		const doorName = item.querySelector('label');
+
+		doorName.innerHTML = doors[blockName][modelName].title;
+	});
+};
+
+function writeSelectedItems() {
+	let selectedBlock = document.querySelector('.seleted-block__inner');
+	let selectedItemsHtmlList = {};
+	selectedBlock.innerHTML = null;
+
+	for (key in selectedItemsList) {
+		let item = selectedItemsList[key];
+		let model = item.model;
+		let name = item.name;
+
+		let innerText = `<div class="door__item">
+									<input type="checkbox" class="door__checkbox visually-hidden" name="${doors[model].name}" id="${doors[model][name]}_lb">
+									<label class="door__checkbox-label" for="${doors[model].name}_lb">
+									${doors[model][name].title}
+									</label>
+									<p class="door__price">Ціна: ${doors[model][name].priceRealiz}<span></span></p>
+									<div class="door__btn-block">
+											<button class="door__value-btn door__value-btn--minus">-</button>
+											<input type="number" class="door__amount" value="1">
+									<button class="door__value-btn door__value-btn--plus">+</button>
+									</div>
+								</div>`
+
+		selectedItemsHtmlList[name] = innerText;
+	};
+
+	for (key in selectedItemsHtmlList) {
+		let item = selectedItemsHtmlList[key];
+
+		selectedBlock.innerHTML += item;
+	};
+
+};
